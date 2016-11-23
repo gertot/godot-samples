0d530aaa3d6ee9460019f57089201b946a56a2f1
Gerhard Totz <gerhard.totz@brz.gv.at>
2016-11-23 12:33:26 +0100

    loading bar

--- game/global.gd
+++ game/global.gd
@@ -6,7 +6,14 @@ var activeScene = null
 func _ready():
    #get the active screen
    activeScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() - 1)
-   
+
+func setPreloadedScene(scene):
+	activeScene.queue_free()
+	#create a new instance of the scene
+	activeScene = scene.instance()
+	# add thee scene to root
+	get_tree().get_root().add_child(activeScene)
+
 # function to switch the scene
 func setScene(scene):
    #clean up the active scene
--- game/scenes/splash/splash.gd
+++ game/scenes/splash/splash.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
 var timer = 0
+var interactiveResourceLoader = null
+var progressbar = null
+var stagecount = 0
+var minTimeToShow = 3
 
 func _ready():
 	set_process(true)
+	progressbar = get_node("progress")
+	progressbar.set_value(0)
+	interactiveResourceLoader = ResourceLoader.load_interactive("res://scenes/menu/menu.tscn")
+	stagecount = interactiveResourceLoader.get_stage_count()
 	
 func _process(delta):
-	timer = timer + delta
-	if (timer >= 2):
-		get_node("/root/global").setScene("res://scenes/menu/menu.tscn")
+	timer += delta
+	if (timer >= minTimeToShow):
+		timer = minTimeToShow
+		
+	var status = interactiveResourceLoader.poll()
+	var stage = interactiveResourceLoader.get_stage()
+	if (status == 18):
+		stage = stagecount
+	
+	var percent = (float(stage) + timer) / (stagecount + minTimeToShow) * 100
+	progressbar.set_value(percent)
+	
+	if (percent == 100):
+		get_node("/root/global").setPreloadedScene(interactiveResourceLoader.get_resource())
+	
